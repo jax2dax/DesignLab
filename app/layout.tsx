83e2442ecd7b6238d633458721d852cf1df01ed1
Jash <joshuabelay9922@gmail.com>
2026-09-10 23:16:33 -0700

    -

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,12 +30,12 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">{children}
         <script 
-          src="http://localhost:3000/tracker.js" 
+          src="http://jellyhookapp.vercel.app/tracker.js" 
           data-key="4b7f327c-e3d7-48ba-b3c5-7fb8db06f5df"
          // strategy="afterInteractive" 
         />
         <Script 
-          src="http://localhost:3000/tracker.js" 
+          src="http://jellyhookapp.vercel.app/tracker.js" 
           data-key="4b7f327c-e3d7-48ba-b3c5-7fb8db06f5df"
          // strategy="afterInteractive" 
         />
