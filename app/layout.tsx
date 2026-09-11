c6c7cea7b48b5ceabed622b2de52e9f4cfd264ef
Jash <joshuabelay9922@gmail.com>
2026-09-10 23:57:37 -0700

    -

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,9 +29,13 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}
-        <script src="jellyhookapp.vercel.app/tracker.js" data-key="0c22a7db-61f8-47bc-bc01-290bdfe03a02"></script>
-        <Script src="jellyhookapp.vercel.app/tracker.js" data-key="0c22a7db-61f8-47bc-bc01-290bdfe03a02"></Script>
-        
+        <script src="https://jellyhookapp.vercel.app/tracker.js" data-key="0c22a7db-61f8-47bc-bc01-290bdfe03a02"></script>
+        <Script src="https://jellyhookapp.vercel.app/tracker.js" data-key="0c22a7db-61f8-47bc-bc01-290bdfe03a02"></Script>
+        <Script
+          src="https://jellyhookapp.vercel.app/tracker.js"
+          data-key="0c22a7db-61f8-47bc-bc01-290bdfe03a02"
+          strategy="afterInteractive"
+        />
       </body>
       
       
